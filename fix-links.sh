22fe8a18926f3774b94e3345ef2c3117def5b38c
Peter Brightwell <peter.brightwell@bbc.co.uk>
2019-01-02 13:35:21 +0000

    Fix space->underscore links in API html

--- fix-links.sh
+++ fix-links.sh
@@ -17,6 +17,18 @@ for file in {branches,tags}/*/docs/*.md; do
 
 done
 
+echo "Fixing links in HTML rendered APIs"
+
+for file in {branches,tags}/*/html-APIs/*.html; do
+
+    # Change .md links to .html
+    perl -pi -e 's:\.md">:.html">:g;' "$file"
+
+    # Change spaces in links to understores
+    perl -ni -e '@parts = split /(href="..\/docs\/.*?\.html)/; for ($n = 1; $n < @parts; $n += 2) { $parts[$n] =~ s/ /_/g; }; print @parts' "$file"
+
+done
+
 ## Removing the unwanted "schemas/" in .html links due to raml2html v6 workaround
 #for file in {branches,tags}/*/html-APIs/*.html; do
 #    perl -pi -e 's:schemas/::g;' "$file"
